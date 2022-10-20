5eecf7bd2f9c0f527331f880661c84cd25e18cbf
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-20 21:26:51 +0600

    Create Login and Registration Form using Daisy UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div>
            <div className="navbar bg-primary text-primary-content">
-  <Link className="btn btn-ghost normal-case text-2xl">daisyUI</Link>
+  <Link className="btn btn-ghost normal-case text-xl'">Awesome Auth</Link>
   <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
   <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
   <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
